573f2f866dea20c988a67beb8b7afc3f23096a51
DeazOut <chupichuros10akk@yandex.ru>
2024-01-14 12:31:53 +0300

    vtoroe

---
+++ src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Здесь будет информация о проекте</h1>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Добро пожаловать на самый лучший сайт!</h1>
+  </div>
+</template>
